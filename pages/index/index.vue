3281ff1defb66cf25f4ede96d39d4a17ac0bc5ea
bb798sky <1632534087@qq.com>
2019-05-28 22:06:16 +0800

    完全改版

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,24 +1,27 @@
 <template>
 	<view class="container">
-		<view class="skuBox">
-			<view class="color" v-for="(item,index) in skuData" :key="index">
+		<view class="skuBox" v-if="guigedata">
+			<view class="color" v-for="(specs, n) in guigedata.apec_attr" :key="n">
 				<view class="type_contnent_box">
 					<view class="title">
-						{{item.attr_name}}
+						{{ specs.attr_name }}
 					</view>
 					<view class="color_item_container">
-						<view class="color_item" 
-						:class="{'active':isActive[index]==index+''+typeIndex}" 
-						:data-id="index+''+typeIndex" 
-						 v-for="(typeItem,typeIndex) in item.attrs" :key="typeIndex" 
-						@tap="clickSku($event,typeIndex,typeItem,index)">
-							{{typeItem.attr_name}}
+						<view class="color_item" :class="[
+							oItem.isShow ? '' : 'disable',
+							subIndex[n] == index ? 'active' : ''
+						  ]"
+						 :data-id="index + '' + index" 
+						 :data-sku="oItem.attr_name" 
+						 :data-skucode="oItem.id" 
+						 v-for="(oItem, index) in specs.attrs"
+						 :key="index" @click="specificationBtn(oItem.id, n, $event, index)">
+							{{ oItem.attr_name }}
 						</view>
-					</view>	
+					</view>
 				</view>
 			</view>
 		</view>
-		<navigator  url="../noStock/index"><button type="primary">没有库存的情况</button></navigator>
 	</view>
 </template>
 
@@ -26,53 +29,115 @@
 	export default {
 		data() {
 			return {
-
-				isActive: [],
-
-				skuData: [],
-				
-				arr: []
-
-			}
+				// 01.规格选择
+				selectArr: [], //存放被选中的值
+				shopItemInfo: {}, //存放要和选中的值进行匹配的数据
+				subIndex: [], //是否选中 因为不确定是多规格还是但规格，所以这里定义数组来判断
+				// 02.规格显示
+				skuShow: null,
+				guigedata: {},
+			};
 		},
-		methods: {
-			clickSku(e, typeIndex, typeItem, index) {
-				console.log('typeitem', e, '规格列表index', index)
-				/* 这里是两层数据 */
-				// let arr = []
-				for (let i = 0, len = this.skuData.length; i < len; i++) {
-					/* 每条规格项里的规格子项 */
-					for (let j = 0, len = this.skuData[i].attrs.length; j < len; j++) {
-						if (e.target.dataset.id == i + '' + j) {
-							this.arr[i] = i + '' + j
-							this.isActive = this.arr.concat()
-						}
-					}
-				}
-				console.log('外层个位内层十位', this.isActive, this.arr)
-			},
-		},
+
 		onLoad() {
 			uni.request({
-				url: 'https://www.easy-mock.com/mock/5cb08b01feb0256836781213/selectSkuData',
+				url: 'https://www.easy-mock.com/mock/5cdd6b00fd487e51be5eb0a0/',
 				data: {},
 				header: {},
 				success: (res) => {
-					console.log(res.data);
-					this.skuData = res.data
+					this.guigedata = res.data.guige_data
+					this.setData()
 				},
 				fail: (err) => {
 					console.log(err)
 				}
 			});
-		}
+		},
 
+		methods: {
+			setData() {
+				let sku = this.guigedata.sku;
+				for (let i = 0, len = sku.length; i < len; i++) {
+					this.shopItemInfo[sku[i].attr_ids.replace(/\//g, ",")] = sku[i];
+					 /* 
+					 * 修改数据结构格式，用规格组成的id用逗号分割作属性名
+					 *  */
+				}
+				this.checkItem();
+			},
+
+			// 01.规格选择
+			specificationBtn(item, n, event, index) {
+				/* 
+				 * n是外层循环，index是内层循环
+				 * item是数据项
+				 * event事件对象
+				 *  */
+				if (this.selectArr[n] != item) {
+					/* 
+					* n是外层索引，也就是颜色、尺寸这样的规格类别
+					* (this.selectArr[n] != item) 也就是这一类规格没有再点击一样的
+					*  */
+					this.selectArr[n] = item;
+					this.subIndex[n] = index;
+				} else {
+					/* 
+					* 如果是点击了这一类里刚点击了的规格，则去除选中态
+					* 也就是点击了选中再点击去除选中
+					* */
+					this.selectArr[n] = "";
+					this.subIndex[n] = -1; //去掉选中的颜色
+				}
+				this.checkItem();
+			},
+			checkItem() {
+				let option = this.guigedata.apec_attr;
+				let result = []; //定义数组存储已经被选中的值
+				let len = option.length;
+				for (let i = 0; i < len; i++) {
+					result[i] = this.selectArr[i] ? this.selectArr[i] : "";
+				}
+				for (let i = 0; i < len; i++) {
+					let last = result[i]; //把选中的值存放到字符串last去
+					let leng = option[i].attrs.length;
+					for (let k = 0; k < leng; k++) {
+						result[i] = option[i].attrs[k].id; //赋值，存在直接覆盖，不存在往里面添加id值
+						/* 
+						* 循环判断每一个规格项
+						* 在数据里面添加字段isShow来判断是否可以选择
+						* 
+						*  */
+						option[i].attrs[k].isShow = this.isMay(result);
+						/* 
+						*  因为this.guigedata.apec_attr是一个对象，
+						* option是指向这个对象的引用
+						* 对option数据改动this.guigedata.apec_attr也会跟着改动
+						* 用this.guigedata.apec_attr来渲染的oItem项数据也跟着变动
+						* 
+						*  */
+						// console.log(this.guigedata.apec_attr[i].attrs[k].isShow,'===',option[i].attrs[k].isShow)
+					}
+					result[i] = last; //还原，目的是记录点下去那个值，避免下一次执行循环时避免被覆盖
+				}
+				this.$forceUpdate(); //重绘
+			},
+			isMay(result) {
+				console.log(result)
+				let len = result.length;
+				for (let i = 0; i < len; i++) {
+					if (result[i] == "") {
+						return true; //如果数组里有为空的值，那直接返回true
+					}
+				}
+				return this.shopItemInfo[result].stock == 0 ? false : true; //匹配选中的数据的库存，若不为空返回true反之返回false
+			},
+		},
 	}
 </script>
 
 <style lang="scss" scoped>
 	.skuBox {
-		height: 400upx;
+		height: 800upx;
 		overflow-y: scroll;
 
 		.color {
@@ -108,6 +173,12 @@
 				.active {
 					background: peachpuff;
 					border: 1upx solid #E94D68;
+				}
+
+				.disable {
+					pointer-events: none;
+					background: lightgray;
+					border: #444444;
 				}
 			}
 
--- unpackage/dist/dev/mp-weixin/pages/noStock/index.js
+++ unpackage/dist/dev/mp-weixin/pages/noStock/index.js
@@ -141,28 +141,16 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
-
-
-
-
-
-
-
-
-
 {
   data: function data() {
     return {
-      guigedata: {},
-      /* 没有库存的商品规格 */
-      noStockSku: [],
-
-      isActive: [],
-
-      disable: [],
-
-      disableBtn: [] };
+      // 01.规格选择
+      selectArr: [], //存放被选中的值
+      shopItemInfo: {}, //存放要和选中的值进行匹配的数据
+      subIndex: [], //是否选中 因为不确定是多规格还是但规格，所以这里定义数组来判断
+      // 02.规格显示
+      skuShow: null,
+      guigedata: {} };
 
   },
 
@@ -173,67 +161,90 @@ __webpack_require__.r(__webpack_exports__);
       header: {},
       success: function success(res) {
         _this.guigedata = res.data.guige_data;
-        setData();
+        _this.setData();
       },
       fail: function fail(err) {
         console.log(err);
       } });
 
-    var setData = function setData() {
-      var sku = _this.guigedata.sku;
-      for (var i = 0, len = sku.length; i < len; i++) {
-        /* 库存为0的规格放入 */
-        if (sku[i].stock == 0) {
-          _this.noStockSku.push({
-            attr_ids: sku[i].attr_ids,
-            attr_name: sku[i].attr_name });
-
-        }
-      }
-    };
   },
 
   methods: {
-    needDisableSku: function needDisableSku(typeItem) {var _this2 = this;
-      var needDisableSkuCode = [];
-      this.noStockSku.forEach(function (item) {
-        var attr_name_arr = item.attr_name.split("/"),
-        attr_ids_arr = item.attr_ids.split("/");
-        if (attr_name_arr.includes(typeItem.attr_name)) {
-          attr_ids_arr.forEach(function (item) {
-            if (item != typeItem.id) {
-              needDisableSkuCode.push(item);
-            }
-          });
-        }
-        function uniq(array) {/* 数组去重 */
-          var temp = [];
-          for (var i = 0; i < array.length; i++) {
-            if (temp.indexOf(array[i]) == -1) {
-              temp.push(array[i]);
-            }
-          }
-          return temp;
-        }
-        needDisableSkuCode = uniq(needDisableSkuCode);
-        _this2.disableBtn = needDisableSkuCode;
-      });
+    setData: function setData() {
+      var sku = this.guigedata.sku;
+      for (var i = 0, len = sku.length; i < len; i++) {
+        this.shopItemInfo[sku[i].attr_ids.replace(/\//g, ",")] = sku[i];
+        /* 
+                                                                         * 修改数据结构格式，用规格组成的id用逗号分割作属性名
+                                                                         *  */
+      }
+      this.checkItem();
     },
-    beSelectedSku: function beSelectedSku(e) {
-      for (var i = 0, len = this.guigedata.apec_attr.length; i < len; i++) {
-        for (
-        var j = 0, _len = this.guigedata.apec_attr[i].attrs.length; j < _len; j++)
-        {
-          if (e.target.dataset.id == i + "" + j) {
-            this.isActive[i] = i + "" + j;
-          }
+
+    // 01.规格选择
+    specificationBtn: function specificationBtn(item, n, event, index) {
+      /* 
+                                                                         * n是外层循环，index是内层循环
+                                                                         * item是数据项
+                                                                         * event事件对象
+                                                                         *  */
+      if (this.selectArr[n] != item) {
+        /* 
+                                      * n是外层索引，也就是颜色、尺寸这样的规格类别
+                                      * (this.selectArr[n] != item) 也就是这一类规格没有再点击一样的
+                                      *  */
+        this.selectArr[n] = item;
+        this.subIndex[n] = index;
+      } else {
+        /* 
+              * 如果是点击了这一类里刚点击了的规格，则去除选中态
+              * 也就是点击了选中再点击去除选中
+              * */
+        this.selectArr[n] = "";
+        this.subIndex[n] = -1; //去掉选中的颜色
+      }
+      this.checkItem();
+    },
+    checkItem: function checkItem() {
+      var option = this.guigedata.apec_attr;
+      var result = []; //定义数组存储已经被选中的值
+      var len = option.length;
+      for (var i = 0; i < len; i++) {
+        result[i] = this.selectArr[i] ? this.selectArr[i] : "";
+      }
+      for (var _i = 0; _i < len; _i++) {
+        var last = result[_i]; //把选中的值存放到字符串last去
+        var leng = option[_i].attrs.length;
+        for (var k = 0; k < leng; k++) {
+          result[_i] = option[_i].attrs[k].id; //赋值，存在直接覆盖，不存在往里面添加id值
+          /* 
+          * 循环判断每一个规格项
+          * 在数据里面添加字段isShow来判断是否可以选择
+          * 
+          *  */
+          option[_i].attrs[k].isShow = this.isMay(result);
+          /* 
+                                                           *  因为this.guigedata.apec_attr是一个对象，
+                                                           * option是指向这个对象的引用
+                                                           * 对option数据改动this.guigedata.apec_attr也会跟着改动
+                                                           * 用this.guigedata.apec_attr来渲染的oItem项数据也跟着变动
+                                                           * 
+                                                           *  */
+          // console.log(this.guigedata.apec_attr[i].attrs[k].isShow,'===',option[i].attrs[k].isShow)
+        }
+        result[_i] = last; //还原，目的是记录点下去那个值，避免下一次执行循环时避免被覆盖
+      }
+      this.$forceUpdate(); //重绘
+    },
+    isMay: function isMay(result) {
+      console.log(result);
+      var len = result.length;
+      for (var i = 0; i < len; i++) {
+        if (result[i] == "") {
+          return true; //如果数组里有为空的值，那直接返回true
         }
       }
-    },
-    clickGuige: function clickGuige(e, typeItem) {
-      if (this.disableBtn.includes(typeItem.id.toString())) return;
-      this.needDisableSku(typeItem);
-      this.beSelectedSku(e);
+      return this.shopItemInfo[result].stock == 0 ? false : true; //匹配选中的数据的库存，若不为空返回true反之返回false
     } } };exports.default = _default;
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ "./node_modules/@dcloudio/uni-mp-weixin/dist/index.js")["default"]))
 
@@ -270,47 +281,38 @@ var render = function() {
       ? _c(
           "view",
           { staticClass: "skuBox" },
-          _vm._l(_vm.guigedata.apec_attr, function(item, index) {
-            return _c("view", { key: index, staticClass: "color" }, [
+          _vm._l(_vm.guigedata.apec_attr, function(specs, n) {
+            return _c("view", { key: n, staticClass: "color" }, [
               _c("view", { staticClass: "type_contnent_box" }, [
                 _c("view", { staticClass: "title" }, [
-                  _vm._v(_vm._s(item.attr_name))
+                  _vm._v(_vm._s(specs.attr_name))
                 ]),
                 _c(
                   "view",
                   { staticClass: "color_item_container" },
-                  _vm._l(item.attrs, function(typeItem, typeIndex) {
+                  _vm._l(specs.attrs, function(oItem, index) {
                     return _c(
                       "view",
                       {
-                        key: typeIndex,
+                        key: index,
                         staticClass: "color_item",
-                        class: {
-                          active: _vm.isActive[index] == index + "" + typeIndex,
-                          disable:
-                            (typeItem.id == _vm.disableBtn[0] &&
-                              _vm.isActive[index] != index + "" + typeIndex) ||
-                            (typeItem.id == _vm.disableBtn[1] &&
-                              _vm.isActive[index] != index + "" + typeIndex) ||
-                            (typeItem.id == _vm.disableBtn[2] &&
-                              _vm.isActive[index] != index + "" + typeIndex) ||
-                            (typeItem.id == _vm.disableBtn[3] &&
-                              _vm.isActive[index] != index + "" + typeIndex) ||
-                            (typeItem.id == _vm.disableBtn[4] &&
-                              _vm.isActive[index] != index + "" + typeIndex)
-                        },
+                        class: [
+                          oItem.isShow ? "" : "disable",
+                          _vm.subIndex[n] == index ? "active" : ""
+                        ],
                         attrs: {
-                          "data-id": index + "" + typeIndex,
-                          "data-sku": typeItem.attr_name,
-                          eventid: "2f159b6d-0-" + index + "-" + typeIndex
+                          "data-id": index + "" + index,
+                          "data-sku": oItem.attr_name,
+                          "data-skucode": oItem.id,
+                          eventid: "2f159b6d-0-" + n + "-" + index
                         },
                         on: {
-                          tap: function($event) {
-                            _vm.clickGuige($event, typeItem)
+                          click: function($event) {
+                            _vm.specificationBtn(oItem.id, n, $event, index)
                           }
                         }
                       },
-                      [_vm._v(_vm._s(typeItem.attr_name))]
+                      [_vm._v(_vm._s(oItem.attr_name))]
                     )
                   })
                 )
